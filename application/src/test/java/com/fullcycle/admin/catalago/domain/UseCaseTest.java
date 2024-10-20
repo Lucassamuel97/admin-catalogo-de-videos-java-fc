18269ad79a4af039a5460642c3f81adf4dab9d66
Lucassamuel97 <lukassamuka88@gmail.com>
2024-10-20 20:55:53 -0300

    :heavy_plus_sign: build: adicionando o Mockito para mockar comportamentos

--- application/src/test/java/com/fullcycle/admin/catalago/domain/UseCaseTest.java
+++ application/src/test/java/com/fullcycle/admin/catalago/domain/UseCaseTest.java
@@ -3,15 +3,15 @@ package com.fullcycle.admin.catalago.domain;
 import com.fullcycle.admin.catalago.application.UseCase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class UseCaseTest {
 
     @Test
     public void testCreateUseCase(){
-        UseCase caso = new UseCase();
-        Assertions.assertNotNull(caso);
 
-        Assertions.assertNotNull(caso.execute());
     }
 
 }
