dc43205008afe5deb1a2799f052e7dccddc44c08
Lucassamuel97 <lukassamuka88@gmail.com>
2024-10-21 21:34:42 -0300

    :heavy_plus_sign: build: adicionando o SpringBoot na camada de infrastructure

--- infrastructure/src/main/java/com/fullcycle/admin/catalago/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalago/infrastructure/Main.java
@@ -1,11 +1,16 @@
 package com.fullcycle.admin.catalago.infrastructure;
 
-import com.fullcycle.admin.catalago.application.UseCase;
+import com.fullcycle.admin.catalago.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
 
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        SpringApplication.run(WebServerConfig.class, args);
 
     }
 }
