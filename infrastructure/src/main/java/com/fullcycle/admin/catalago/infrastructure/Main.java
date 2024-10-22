36ab5d9670577ba0c29f06579bfb2cbb6dd6aa21
Lucassamuel97 <lukassamuka88@gmail.com>
2024-10-22 09:33:20 -0300

    :wrench: chore: configurando os profiles do springboot

--- infrastructure/src/main/java/com/fullcycle/admin/catalago/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalago/infrastructure/Main.java
@@ -3,12 +3,16 @@ package com.fullcycle.admin.catalago.infrastructure;
 import com.fullcycle.admin.catalago.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
+
+        //System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "development");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
 
         SpringApplication.run(WebServerConfig.class, args);
 
