6ad0e5c609483694c033bb09e935f20e0b2c0e1d
Lucassamuel97 <lukassamuka88@gmail.com>
2024-10-25 16:13:46 -0300

    :test_tube: test: TDD, Adiciona testes de integração para validação do endpoint de Get de categoria por id

--- infrastructure/src/test/java/com/fullcycle/admin/catalago/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalago/infrastructure/api/CategoryAPITest.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fullcycle.admin.catalago.ControllerTest;
 import com.fullcycle.admin.catalago.application.category.create.CreateCategoryOutput;
 import com.fullcycle.admin.catalago.application.category.create.CreateCategoryUseCase;
+import com.fullcycle.admin.catalago.application.category.retrieve.get.CategoryOutput;
+import com.fullcycle.admin.catalago.application.category.retrieve.get.GetCategoryByIdUseCase;
 import com.fullcycle.admin.catalago.domain.exceptions.DomainException;
+import com.fullcycle.admin.catalago.domain.category.Category;
 import com.fullcycle.admin.catalago.domain.validation.handler.Notification;
 import com.fullcycle.admin.catalago.infrastructure.category.models.CreateCategoryApiInput;
 import org.hamcrest.Matchers;
@@ -38,6 +41,9 @@ public class CategoryAPITest {
 
     @MockBean
     private CreateCategoryUseCase createCategoryUseCase;
+
+    @MockBean
+    private GetCategoryByIdUseCase getCategoryByIdUseCase;
 
     @Test
     public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() throws Exception {
@@ -144,6 +150,43 @@ public class CategoryAPITest {
                         && Objects.equals(expectedDescription, cmd.description())
                         && Objects.equals(expectedIsActive, cmd.isActive())
         ));
+    }
+
+    @Test
+    public void givenAValidId_whenCallsGetCategory_shouldReturnCategory() throws Exception {
+        // given
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var aCategory =
+                Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var expectedId = aCategory.getId().getValue();
+
+        when(getCategoryByIdUseCase.execute(any()))
+                .thenReturn(CategoryOutput.from(aCategory));
+
+        // when
+        final var request = MockMvcRequestBuilders.get("/categories/{id}", expectedId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        final var response = this.mvc.perform(request)
+                .andDo(print());
+
+        // then
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id", Matchers.equalTo(expectedId)))
+                .andExpect(jsonPath("$.name", Matchers.equalTo(expectedName)))
+                .andExpect(jsonPath("$.description", Matchers.equalTo(expectedDescription)))
+                .andExpect(jsonPath("$.is_active", Matchers.equalTo(expectedIsActive)))
+                .andExpect(jsonPath("$.created_at", Matchers.equalTo(aCategory.getCreatedAt().toString())))
+                .andExpect(jsonPath("$.updated_at", Matchers.equalTo(aCategory.getUpdatedAt().toString())))
+                .andExpect(jsonPath("$.deleted_at", Matchers.equalTo(aCategory.getDeletedAt())));
+
+        verify(getCategoryByIdUseCase, times(1)).execute(eq(expectedId));
     }
 
 }
