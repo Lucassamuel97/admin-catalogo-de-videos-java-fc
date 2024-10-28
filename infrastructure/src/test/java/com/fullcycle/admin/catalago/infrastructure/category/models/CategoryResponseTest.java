14c9446f1d2b71adcd6bdb35988ee96bdc55fd6b
Lucassamuel97 <lukassamuka88@gmail.com>
2024-10-28 13:59:08 -0300

    :wrench: chore: Configurando estratégia para testes integrados de serialização de classes

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalago/infrastructure/category/models/CategoryResponseTest.java
@@ -0,0 +1,16 @@
+package com.fullcycle.admin.catalago.infrastructure.category.models;
+
+import com.fullcycle.admin.catalago.JacksonTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+@JacksonTest
+public class CategoryResponseTest {
+
+    @Autowired
+    private JacksonTester<CategoryResponse> json;
+
+    public void t(){
+
+    }
+}
