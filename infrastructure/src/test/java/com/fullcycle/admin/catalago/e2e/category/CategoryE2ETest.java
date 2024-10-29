f2f9eb16bef7efb2a2057401cb71635c1082db4d
Lucassamuel97 <lukassamuka88@gmail.com>
2024-10-29 16:04:04 -0300

    :test_tube: test: Adiciona testes e2e da criação de categoria

--- infrastructure/src/test/java/com/fullcycle/admin/catalago/e2e/category/CategoryE2ETest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalago/e2e/category/CategoryE2ETest.java
@@ -1,10 +1,20 @@
 package com.fullcycle.admin.catalago.e2e.category;
 
 import com.fullcycle.admin.catalago.E2ETest;
+import com.fullcycle.admin.catalago.domain.category.CategoryID;
+import com.fullcycle.admin.catalago.infrastructure.category.models.CategoryResponse;
+import com.fullcycle.admin.catalago.infrastructure.category.models.CreateCategoryRequest;
+import com.fullcycle.admin.catalago.infrastructure.category.persistence.CategoryRepository;
+import com.fullcycle.admin.catalago.infrastructure.configuration.json.Json;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -14,6 +24,12 @@ import java.time.Duration;
 @E2ETest
 @Testcontainers
 public class CategoryE2ETest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Container
     private static final MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>("mysql:8.0")
@@ -29,7 +45,53 @@ public class CategoryE2ETest {
     }
 
     @Test
-    public void testWorks(){
+    public void asACatalogAdminIShouldBeAbleToCreateANewCategoryWithValidValues() throws Exception {
         Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualId = givenACategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualCategory = retrieveACategory(actualId.getValue());
+
+        Assertions.assertEquals(expectedName, actualCategory.name());
+        Assertions.assertEquals(expectedDescription, actualCategory.description());
+        Assertions.assertEquals(expectedIsActive, actualCategory.active());
+        Assertions.assertNotNull(actualCategory.createdAt());
+        Assertions.assertNotNull(actualCategory.updatedAt());
+        Assertions.assertNull(actualCategory.deletedAt());
+    }
+
+    private CategoryID givenACategory(final String aName, final String aDescription, final boolean isActive) throws Exception{
+        final var requestBody = new CreateCategoryRequest(aName,aDescription,isActive);
+
+        final var aRequest = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Json.writeValueAsString(requestBody));
+
+        final var actualId = this.mvc.perform(aRequest)
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andReturn()
+                .getResponse().getHeader("Location")
+                .replace("/categories/","");
+
+        return CategoryID.from(actualId);
+    }
+
+    private CategoryResponse retrieveACategory(final String anId) throws Exception {
+
+        final var aRequest = MockMvcRequestBuilders.get("/categories/"+ anId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        final var json = this.mvc.perform(aRequest)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn()
+                .getResponse().getContentAsString();
+
+        return Json.readValue(json, CategoryResponse.class);
     }
 }
