87d107e10631eeabf729569ab3009e4be856a0a5
Lucassamuel97 <lukassamuka88@gmail.com>
2024-10-31 11:18:41 -0300

    :sparkles: feat: implementando o comportamento e testes para update de um genre

--- domain/src/main/java/com/fullcycle/admin/catalago/domain/genre/Genre.java
+++ domain/src/main/java/com/fullcycle/admin/catalago/domain/genre/Genre.java
@@ -76,6 +76,19 @@ public class Genre extends AggregateRoot<GenreID> {
         new GenreValidator(this, handler).validate();
     }
 
+    public Genre update(final String aName, final boolean isActive, final List<CategoryID> categories) {
+        if (isActive) {
+            activate();
+        } else {
+            deactivate();
+        }
+        this.name = aName;
+        this.categories = new ArrayList<>(categories != null ? categories : Collections.emptyList());
+        this.updatedAt = InstantUtils.now();
+        selfValidate();
+        return this;
+    }
+
     public Genre deactivate() {
         if (getDeletedAt() == null) {
             this.deletedAt = InstantUtils.now();
