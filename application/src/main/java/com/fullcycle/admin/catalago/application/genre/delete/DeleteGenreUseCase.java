a39bef7065097bf683ba1fe3f62af104492edcc4
Lucassamuel97 <lukassamuka88@gmail.com>
2024-11-08 23:00:22 -0300

    :sparkles: feat: implementando o caso de uso de delete de genre por id

---
+++ application/src/main/java/com/fullcycle/admin/catalago/application/genre/delete/DeleteGenreUseCase.java
@@ -0,0 +1,6 @@
+package com.fullcycle.admin.catalago.application.genre.delete;
+
+import com.fullcycle.admin.catalago.application.UnitUseCase;
+
+public abstract class DeleteGenreUseCase extends UnitUseCase<String> {
+}
