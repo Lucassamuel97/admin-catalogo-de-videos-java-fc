40404b879f735486fd94f968f5398ecddd0af716
Lucassamuel97 <lukassamuka88@gmail.com>
2024-11-29 15:40:24 -0300

    :sparkles: feat: Implementado o caso de uso de delete de vídeo por id

---
+++ application/src/main/java/com/fullcycle/admin/catalago/application/video/delete/DeleteVideoUseCase.java
@@ -0,0 +1,7 @@
+package com.fullcycle.admin.catalago.application.video.delete;
+
+
+import com.fullcycle.admin.catalago.application.UnitUseCase;
+
+public abstract class DeleteVideoUseCase extends UnitUseCase<String> {
+}
