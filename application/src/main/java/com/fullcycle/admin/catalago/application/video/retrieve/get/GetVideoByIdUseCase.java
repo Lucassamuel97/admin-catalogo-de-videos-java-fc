84fe4993d8a284f233594031210c1401d6f188f8
Lucassamuel97 <lukassamuka88@gmail.com>
2024-11-29 16:32:06 -0300

    :sparkles: feat: Implementado o caso de uso de Get de vídeo por id

---
+++ application/src/main/java/com/fullcycle/admin/catalago/application/video/retrieve/get/GetVideoByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.fullcycle.admin.catalago.application.video.retrieve.get;
+
+import com.fullcycle.admin.catalago.application.UseCase;
+
+public abstract class GetVideoByIdUseCase extends UseCase<String, VideoOutput> {
+}
