f3199beaa0f69dcfe479714bd9fb544e663cbf7c
Lucassamuel97 <lukassamuka88@gmail.com>
2024-11-29 16:39:08 -0300

    :test_tube: test: TDD, testes unitários do caso de uso de Atualização (update) de vídeo

---
+++ application/src/test/java/com/fullcycle/admin/catalago/application/video/update/UpdateVIdeoUseCaseTest.java
@@ -0,0 +1,156 @@
+package com.fullcycle.admin.catalago.application.video.update;
+
+import com.fullcycle.admin.catalago.application.Fixture;
+import com.fullcycle.admin.catalago.application.UseCaseTest;
+import com.fullcycle.admin.catalago.domain.castmember.CastMemberGateway;
+import com.fullcycle.admin.catalago.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalago.domain.genre.GenreGateway;
+import com.fullcycle.admin.catalago.domain.video.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.time.Year;
+import java.util.*;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+public class UpdateVIdeoUseCaseTest extends UseCaseTest {
+
+    @InjectMocks
+    private DefaultUpdateVideoUseCase useCase;
+
+    @Mock
+    private VideoGateway videoGateway;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    @Mock
+    private GenreGateway genreGateway;
+
+    @Mock
+    private CastMemberGateway castMemberGateway;
+
+    @Mock
+    private MediaResourceGateway mediaResourceGateway;
+
+    @Override
+    protected List<Object> getMocks() {
+        return List.of(videoGateway, categoryGateway, genreGateway, castMemberGateway, mediaResourceGateway);
+    }
+
+    @Test
+    public void givenAValidCommand_whenCallsUpdateVideo_shouldReturnVideoId() {
+        // given
+        final var aVideo = Fixture.Videos.systemDesign();
+
+        final var expectedTitle = Fixture.title();
+        final var expectedDescription = Fixture.Videos.description();
+        final var expectedLaunchYear = Year.of(Fixture.year());
+        final var expectedDuration = Fixture.duration();
+        final var expectedOpened = Fixture.bool();
+        final var expectedPublished = Fixture.bool();
+        final var expectedRating = Fixture.Videos.rating();
+        final var expectedCategories = Set.of(Fixture.Categories.aulas().getId());
+        final var expectedGenres = Set.of(Fixture.Genres.tech().getId());
+        final var expectedMembers = Set.of(
+                Fixture.CastMembers.wesley().getId(),
+                Fixture.CastMembers.samuca().getId()
+        );
+        final Resource expectedVideo = Fixture.Videos.resource(VideoMediaType.VIDEO);
+        final Resource expectedTrailer = Fixture.Videos.resource(VideoMediaType.TRAILER);
+        final Resource expectedBanner = Fixture.Videos.resource(VideoMediaType.BANNER);
+        final Resource expectedThumb = Fixture.Videos.resource(VideoMediaType.THUMBNAIL);
+        final Resource expectedThumbHalf = Fixture.Videos.resource(VideoMediaType.THUMBNAIL_HALF);
+
+        final var aCommand = UpdateVideoCommand.with(
+                aVideo.getId().getValue(),
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchYear.getValue(),
+                expectedDuration,
+                expectedOpened,
+                expectedPublished,
+                expectedRating.getName(),
+                asString(expectedCategories),
+                asString(expectedGenres),
+                asString(expectedMembers),
+                expectedVideo,
+                expectedTrailer,
+                expectedBanner,
+                expectedThumb,
+                expectedThumbHalf
+        );
+
+        when(videoGateway.findById(any()))
+                .thenReturn(Optional.of(Video.with(aVideo)));
+
+        when(categoryGateway.existsByIds(any()))
+                .thenReturn(new ArrayList<>(expectedCategories));
+
+        when(castMemberGateway.existsByIds(any()))
+                .thenReturn(new ArrayList<>(expectedMembers));
+
+        when(genreGateway.existsByIds(any()))
+                .thenReturn(new ArrayList<>(expectedGenres));
+
+        mockImageMedia();
+        mockAudioVideoMedia();
+
+        when(videoGateway.update(any()))
+                .thenAnswer(returnsFirstArg());
+
+        // when
+        final var actualResult = useCase.execute(aCommand);
+
+        // then
+        Assertions.assertNotNull(actualResult);
+        Assertions.assertNotNull(actualResult.id());
+
+        verify(videoGateway).findById(eq(aVideo.getId()));
+
+        verify(videoGateway).update(argThat(actualVideo ->
+                Objects.equals(expectedTitle, actualVideo.getTitle())
+                        && Objects.equals(expectedDescription, actualVideo.getDescription())
+                        && Objects.equals(expectedLaunchYear, actualVideo.getLaunchedAt())
+                        && Objects.equals(expectedDuration, actualVideo.getDuration())
+                        && Objects.equals(expectedOpened, actualVideo.getOpened())
+                        && Objects.equals(expectedPublished, actualVideo.getPublished())
+                        && Objects.equals(expectedRating, actualVideo.getRating())
+                        && Objects.equals(expectedCategories, actualVideo.getCategories())
+                        && Objects.equals(expectedGenres, actualVideo.getGenres())
+                        && Objects.equals(expectedMembers, actualVideo.getCastMembers())
+                        && Objects.equals(expectedVideo.name(), actualVideo.getVideo().get().name())
+                        && Objects.equals(expectedTrailer.name(), actualVideo.getTrailer().get().name())
+                        && Objects.equals(expectedBanner.name(), actualVideo.getBanner().get().name())
+                        && Objects.equals(expectedThumb.name(), actualVideo.getThumbnail().get().name())
+                        && Objects.equals(expectedThumbHalf.name(), actualVideo.getThumbnailHalf().get().name())
+                        && Objects.equals(aVideo.getCreatedAt(), actualVideo.getCreatedAt())
+                        && aVideo.getUpdatedAt().isBefore(actualVideo.getUpdatedAt())
+        ));
+    }
+
+    private void mockImageMedia() {
+        when(mediaResourceGateway.storeImage(any(), any())).thenAnswer(t -> {
+            final var resource = t.getArgument(1, Resource.class);
+            return ImageMedia.with(resource.checksum(), resource.name(), "/img");
+        });
+    }
+
+    private void mockAudioVideoMedia() {
+        when(mediaResourceGateway.storeAudioVideo(any(), any())).thenAnswer(t -> {
+            final var resource = t.getArgument(1, Resource.class);
+            return AudioVideoMedia.with(
+                    resource.checksum(),
+                    resource.name(),
+                    "/img"
+            );
+        });
+    }
+}
