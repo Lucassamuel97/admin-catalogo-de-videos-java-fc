ba993ce8bc25ed74189f96418615140e01b390a4
Lucassamuel97 <lukassamuka88@gmail.com>
2025-01-23 21:07:00 -0300

    :sparkles: feat: qualificando os beans para injeção do spring

--- infrastructure/src/main/java/com/fullcycle/admin/catalago/infrastructure/configuration/AmqpConfig.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalago/infrastructure/configuration/AmqpConfig.java
@@ -1,5 +1,7 @@
 package com.fullcycle.admin.catalago.infrastructure.configuration;
 
+import com.fullcycle.admin.catalago.infrastructure.configuration.annotations.VideoCreatedQueue;
+import com.fullcycle.admin.catalago.infrastructure.configuration.annotations.VideoEncodedQueue;
 import com.fullcycle.admin.catalago.infrastructure.configuration.properties.amqp.QueueProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -10,15 +12,21 @@ public class AmqpConfig {
 
     @Bean
     @ConfigurationProperties("amqp.queues.video-created")
+    @VideoCreatedQueue
     QueueProperties videoCreatedQueueProperties() {
         return new QueueProperties();
     }
 
     @Bean
     @ConfigurationProperties("amqp.queues.video-encoded")
+    @VideoEncodedQueue
     QueueProperties videoEncodedQueueProperties() {
         return new QueueProperties();
     }
 
+    @Bean
+    public String queueName(@VideoCreatedQueue QueueProperties props) {
+        return props.getQueue();
+    }
 
 }
