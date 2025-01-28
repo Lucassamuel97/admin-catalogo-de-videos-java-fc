ddef154115473addbd73788a63e50e9942688519
Lucassamuel97 <lukassamuka88@gmail.com>
2025-01-27 22:35:40 -0300

    :test_tube: test: testes unitários de domain events na classe entity

--- domain/src/main/java/com/fullcycle/admin/catalago/domain/event/DomainEventPublisher.java
+++ domain/src/main/java/com/fullcycle/admin/catalago/domain/event/DomainEventPublisher.java
@@ -2,5 +2,5 @@ package com.fullcycle.admin.catalago.domain.event;
 
 @FunctionalInterface
 public interface DomainEventPublisher {
-    <T extends DomainEvent> void publishEvent(T event);
+    void publishEvent(DomainEvent event);
 }
