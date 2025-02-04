aaec5f7691fef40a542e5b1f841fd3ca38e5c975
Lucassamuel97 <lukassamuka88@gmail.com>
2025-02-03 21:24:15 -0300

    :sparkles: feat: Criando controller de video

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalago/infrastructure/api/VideoAPI.java
@@ -0,0 +1,11 @@
+package com.fullcycle.admin.catalago.infrastructure.api;
+
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "videos")
+@Tag(name = "Video")
+public interface VideoAPI {
+
+}
---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalago/infrastructure/api/controllers/VideoController.java
@@ -0,0 +1,9 @@
+package com.fullcycle.admin.catalago.infrastructure.api.controllers;
+
+import com.fullcycle.admin.catalago.infrastructure.api.VideoAPI;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VideoController implements VideoAPI {
+
+}
